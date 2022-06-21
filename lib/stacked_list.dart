433196f228baf6ed5a588f8fea32f7da1692ef74
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-06-21 17:16:48 -0500

    Creating the stateless Widget StackedList which will harbor the body of the UI, in this case, the stack of cards. In the upper part I've already built a simple Appbar component.

---
+++ lib/stacked_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StackedList extends StatelessWidget {
+  const StackedList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
